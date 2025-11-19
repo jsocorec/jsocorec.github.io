5c21556b44f8f1531244eea747861337361d71fb
jsocorec <54444669+jsocorec@users.noreply.github.com>
2025-11-19 17:45:59 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // app.js - utilidades de frontend
 window.__env = {
   //API_BASE_URL: 'http://localhost:8081', // cambiar si corresponde
-  API_BASE_URL: 'http://Ms-clientes-visitas-env2.eba-ie2e5n2t.us-east-1.elasticbeanstalk.com',
+  API_BASE_URL: 'https://Ms-clientes-visitas-env2.eba-ie2e5n2t.us-east-1.elasticbeanstalk.com',
   GOOGLE_MAPS_API_KEY: 'AIzaSyAkxLcCxxcH3u81ofoTGlKGz9zvYW1I8gQ' // opcional, se usa luego
 };
 
@@ -13,4 +13,5 @@ function isAuthenticated(){ return !!getToken(); }
 // simple helper to redirect to login if not auth
 function requireAuth() {
   if (!isAuthenticated()) window.location = '../index.html';
+
 }
